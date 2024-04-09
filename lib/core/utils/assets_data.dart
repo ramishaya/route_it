3de3e6c0de8b143a4050ca3320867d30ca7e7370
ramishaya <ramishaiya8@gmail.com>
2024-04-09 18:35:47 +0300

    homeEdit

--- lib/core/utils/assets_data.dart
+++ lib/core/utils/assets_data.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const avatarImg = "assets/images/hacker.png";
   static const categoryImg = "assets/images/categoryimg3.png";
+  static const categoryImg2 = "assets/images/category_more.png";
 }
