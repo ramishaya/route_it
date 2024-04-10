dfb1aff9cc066e34a90b8e61a95ba2f33afc6a64
ramishaya <ramishaiya8@gmail.com>
2024-04-10 15:49:29 +0300

    homeScreenFinal2

---
+++ lib/core/widgets/default_height.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DefaultHeight extends StatelessWidget {
+  const DefaultHeight({
+    super.key,
+    required this.size,
+    required this.height
+  });
+
+  final double height;
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: size.height * height);
+  }
+}
