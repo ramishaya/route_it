8a5095cf7019c6e7e69ddfed8d3e2b8a8382383c
TaimAmer <ttttamer1111@gmail.com>
2024-04-17 04:54:37 -0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,40 @@ import 'package:route_it/core/utils/app_colors.dart';
 import 'package:route_it/core/utils/app_router.dart';
 import 'package:route_it/core/utils/app_theme.dart';
 import 'package:route_it/core/utils/bloc_observer.dart';
+import 'package:route_it/core/utils/cache_services.dart';
 import 'package:route_it/core/utils/service_locator.dart';
 import 'package:route_it/core/utils/shared_prefrences.dart';
+import 'package:route_it/features/home/presentation/views/home_view.dart';
+import 'package:route_it/features/login/presentation/views/login_view.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
+  // await CacheServices.init();
   setupServiceLocator();
+
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: darkPrimaryColor.withOpacity(0.15),
       statusBarIconBrightness: Brightness.light));
-  SharedPref.init();
+
   // getIt.get<SharedPref>().init();
-  runApp(const RouteIT());
+  // String? token = CacheServices.getData(key: 'token');
+  // Widget? widget;
+  //
+  //   if(token != null) {
+  //     widget = const HomeView();
+  //   } else {
+  //     widget = LoginView();
+  //   }
+  runApp(const RouteIT(
+    // firstView: widget,
+  ));
 }
 
 class RouteIT extends StatelessWidget {
   const RouteIT({super.key});
+
+  // final Widget firstView;
 
   @override
   Widget build(BuildContext context) {
